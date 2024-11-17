c4de2ae17f42f6ad81a154255d58dfbbee1e203b
Gabriel Rodrigues de Souza <bielrodrigues.2406@outlook.com>
2024-11-16 23:03:29 -0300

    add mais classes na DAO

---
+++ src/main/java/br/edu/ifsp/pep/dao/ForncedorDAO.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsp.pep.dao;
+
+import br.edu.ifsp.pep.entity.Fornecedor;
+
+/**
+ *
+ * @author User
+ */
+public class ForncedorDAO extends AbstractDAO<Fornecedor>{
+    
+}
