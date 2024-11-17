566101bbf5e80562498c105e0c7675ccf744d293
Gabriel Rodrigues de Souza <bielrodrigues.2406@outlook.com>
2024-11-16 22:46:00 -0300

    add entity Cliente

---
+++ src/main/java/br/edu/ifsp/pep/entity/Cliente.java
@@ -0,0 +1,141 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsp.pep.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Objects;
+import org.primefaces.component.password.Password;
+
+/**
+ *
+ * @author User
+ */
+public class Cliente {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
+    private Long idCliente;
+    
+    private String primeiroNome;
+    
+    private String ultimoNome;
+    
+    private String cpf;
+    
+    private String telefone;
+    
+    private boolean status = true;
+    
+    private String senha;
+    
+    private Password password; //aprender como mexe nessa bomba
+
+    public Cliente() {
+    }
+
+    public Cliente(Long idCliente, String primeiroNome, String ultimoNome, String cpf, String telefone, String senha, Password password) {
+        this.idCliente = idCliente;
+        this.primeiroNome = primeiroNome;
+        this.ultimoNome = ultimoNome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.senha = senha;
+        this.password = password;
+    }
+
+    
+    
+    public Long getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Long idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getUltimoNome() {
+        return ultimoNome;
+    }
+
+    public void setUltimoNome(String ultimoNome) {
+        this.ultimoNome = ultimoNome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Password getPassword() {
+        return password;
+    }
+
+    public void setPassword(Password password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.idCliente);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        return Objects.equals(this.idCliente, other.idCliente);
+    }
+    
+        
+}
