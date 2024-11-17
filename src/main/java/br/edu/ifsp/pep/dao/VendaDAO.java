6a063b87d0688aa0da4cd38954cf6a209677e820
Gabriel Rodrigues de Souza <bielrodrigues.2406@outlook.com>
2024-11-16 22:32:20 -0300

    add de alguns DAO

---
+++ src/main/java/br/edu/ifsp/pep/dao/VendaDAO.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsp.pep.dao;
+
+import br.edu.ifsp.pep.entity.Venda;
+import jakarta.persistence.TypedQuery;
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ *
+ * @author User
+ */
+public class VendaDAO extends AbstractDAO<Venda> {
+
+    public List<Venda> buscarTodas() {
+        return em.createNamedQuery("Pessoa.buscarTodas", Venda.class)
+                .getResultList();
+
+    }
+
+    public List<Venda> buscarPorData(LocalDate dataInicio, LocalDate dataFim) {
+        TypedQuery<Venda> query = em.createNamedQuery("Venda.buscarPorData", Venda.class);
+        query.setParameter("datainicio", dataInicio);
+        query.setParameter("datafim", dataFim);
+
+        return query.getResultList();
+    }
+}
