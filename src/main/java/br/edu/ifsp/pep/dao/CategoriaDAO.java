a051af4dd0a8d9fa36986b052eac2926a481da76
ojorgg <jorgg.jesus@gmail.com>
2024-12-04 20:47:11 -0300

    feat: crud categoria incompleto

--- src/main/java/br/edu/ifsp/pep/dao/CategoriaDAO.java
+++ src/main/java/br/edu/ifsp/pep/dao/CategoriaDAO.java
@@ -5,13 +5,19 @@
 package br.edu.ifsp.pep.dao;
 
 import br.edu.ifsp.pep.entity.Categoria;
+import br.edu.ifsp.pep.entity.Pessoa;
 import jakarta.ejb.Stateless;
+import java.util.List;
 
 /**
  *
  * @author User
  */
 @Stateless
-public class CategoriaDAO extends AbstractDAO<Categoria>{
-    
+public class CategoriaDAO extends AbstractDAO<Categoria> {
+
+    public List<Categoria> buscarTodas() {
+        return em.createNamedQuery("Categoria.buscaTodas", Categoria.class)
+                .getResultList();
+    }
 }
