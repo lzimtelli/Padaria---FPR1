44f96e6b4dd04a4618ecabb08439cb3ae15b9dd4
Gabriel Rodrigues de Souza <bielrodrigues.2406@outlook.com>
2024-12-05 20:59:54 -0300

    feat: add de busca pelo carrinho do usuario

--- src/main/java/br/edu/ifsp/pep/dao/CarrinhoDAO.java
+++ src/main/java/br/edu/ifsp/pep/dao/CarrinhoDAO.java
@@ -4,13 +4,24 @@
  */
 package br.edu.ifsp.pep.dao;
 
+import br.edu.ifsp.pep.controller.LoginController;
+import br.edu.ifsp.pep.entity.Carrinho;
+import br.edu.ifsp.pep.entity.Pessoa;
 import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+import jakarta.persistence.TypedQuery;
 
 /**
  *
  * @author User
  */
 @Stateless
-public class CarrinhoDAO {
-    
+public class CarrinhoDAO extends AbstractDAO<Carrinho> {
+
+     public Carrinho buscaCarrinhoPessoa(Pessoa pessoa){
+          TypedQuery<Carrinho> query = em.createNamedQuery("Carrinho.buscaAberto", Carrinho.class);
+        query.setParameter("pessoa", pessoa);
+
+        return query.getSingleResult();   
+    } 
 }
